64dab8272d52776af90f0ba5f9ee1230332891e5
wang-coal <wb.1220@163.com>
2024-04-26 19:23:23 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '@/app.css';
+import '@/app.less';
+
+import Class from './components/Class';
+function App() {
+  return (
+    <h2>
+      webpack5-react-ts
+      <Class></Class>
+    </h2>
+  );
+}
+export default App;
